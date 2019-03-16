2d1e820b96ede34b45e11c59f564f74388e07410
Felipe Loureiro <felipe-loureiro@outlook.com>
2019-03-16 20:22:02 -0300

    Commit base

---
+++ src/br/edu/ifsp/spo/sp3013219/aula3/ContaCorrente.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.spo.sp3013219.aula3;
+
+public class ContaCorrente extends Conta{
+	
+	//VARIÁVEIS
+	public double limiteChequeEspecial;
+	public double taxaJurusChequeEspecial;
+	
+	//MÉTODOS
+	public void debitarJuros() {
+		
+	}
+	
+	public void sacar(){
+		
+	}
+}
---
+++ src/br/edu/ifsp/spo/sp3013219/aula3/ContaPoupanca.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.sp3013219.aula3;
+
+public class ContaPoupanca extends Conta{
+
+	//VÁRIAVEIS
+	static double taxaRendimento;
+	
+	//MÉTODOS
+	public void aplicarRendimento() {
+		
+	}
+	
+}
