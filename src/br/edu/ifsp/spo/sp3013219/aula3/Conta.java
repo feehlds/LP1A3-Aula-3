88735f6d78061603515ef14fd0f6aaf829cf8493
Felipe Loureiro <felipe-loureiro@outlook.com>
2019-03-16 20:10:18 -0300

    First Commit

---
+++ src/br/edu/ifsp/spo/sp3013219/aula3/Conta.java
@@ -0,0 +1,22 @@
+package br.edu.ifsp.spo.sp3013219.aula3;
+
+public class Conta {
+	
+	//VARIÁVEIS
+	public int numConta;
+	public String titular;
+	public double saldo;
+	
+	//MÉTODOS
+	public void transferirPara() {
+		
+	}
+
+	public void depositar() {
+		
+	}
+	
+	public void sacar() {
+		
+	}
+}
