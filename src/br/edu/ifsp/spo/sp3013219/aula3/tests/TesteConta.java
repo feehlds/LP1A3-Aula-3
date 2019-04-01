3c17b12fa9d7d940f27a115d6038a65b6f3a012e
Felipe Loureiro <felipe-loureiro@outlook.com>
2019-04-01 20:57:03 -0300

    Exceptions

--- src/br/edu/ifsp/spo/sp3013219/aula3/tests/TesteConta.java
+++ src/br/edu/ifsp/spo/sp3013219/aula3/tests/TesteConta.java
@@ -6,6 +6,38 @@ import br.edu.ifsp.spo.sp3013219.aula3.*;
 public class TesteConta {
 	
 	@Test
+	void instanciar_conta() {
+		//1. Configuração
+		String titular = "José";
+		String num = "404-404";
+		//2. Execução
+		Conta conta = new Conta(num, titular);
+		//3. Validação / Asserção
+		assertNotNull(conta);
+		assertEquals(titular, conta.getTitular());
+		assertEquals(num, conta.getNumConta());	
+	}
+	
+	@Test
+	void tem_que_transferir() {
+		//1. Configuração
+		String titular = "José";
+		String num = "404-404";
+		String titular2 = "Maria";
+		String num2 = "402-402";
+		double transferir = 300.00;
+		//2. Execução
+		Conta conta = new Conta(num, titular);
+		Conta conta2 = new Conta(num2, titular2);
+		conta.setSaldo(600.00);
+		conta2.setSaldo(600.00);
+		conta.transferirPara(conta2, transferir);
+		//3. Validação / Asserção
+		assertEquals(600.00 - 300.00, conta.getSaldo());
+		assertEquals(600.00 + 300.00, conta2.getSaldo());
+	}
+	
+	@Test
 	void exception_titular_nulo() {
 		//1. Configuração
 		String titular = null;
