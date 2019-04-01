b269afb18314b194e61acacca8bc1c3d7f653aac
Felipe Loureiro <felipe-loureiro@outlook.com>
2019-04-01 20:44:50 -0300

    Exceptions

--- src/br/edu/ifsp/spo/sp3013219/aula3/tests/TesteConta.java
+++ src/br/edu/ifsp/spo/sp3013219/aula3/tests/TesteConta.java
@@ -31,4 +31,32 @@ public class TesteConta {
 		}		
 	}
 	
+	@Test
+	void exception_saldo_insuficiente_saque() {
+		//1. Configuração
+		String titular = "José";
+		String num = "404-404";
+		Conta conta = new Conta(num, titular);
+		//2. Execução
+		try {
+			conta.setSaldo(40.00);
+			conta.sacar(50.00);
+		} catch(Exception exc) {
+			System.out.println(exc.getMessage());
+		}
+	}
+	
+	@Test
+	void exception_limite_menor_ou_igual_zero() {
+		//1. Configuração
+		String titular = "José";
+		String num = "404-404";
+		double limite = 0;
+		
+		try {
+			Conta conta = new ContaCorrente(num, titular, limite);
+		}	catch(Exception exc) {
+				System.out.println(exc.getMessage());
+		}
+	}
 }
